9323eaad127ca2d2cb64cf223abadf1227a5a218
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-01-18 19:48:34 +0100

    Create complementary_dna.java

---
+++ complementary_dna.java
@@ -0,0 +1,21 @@
+public class DnaStrand {
+  public static String makeComplement(String dna) {
+    char[] tab = dna.toCharArray();
+    
+    for (int i = 0; i < tab.length; i++) {
+      tab[i] = getComplement(tab[i]);
+    }
+    
+    return new String(tab);
+    }
+    
+    private static char getComplement(char c) {
+      switch (c){
+        case 'A' : return 'T';
+        case 'T' : return 'A';
+        case 'C' : return 'G';
+        case 'G' : return 'C';
+          }
+      return c;
+  }
+}
