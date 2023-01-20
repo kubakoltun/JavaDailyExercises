da8ee42929f81beb342738145a53081f1cdb8cf2
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-01-20 21:59:07 +0100

    Create counting_sheep.java

---
+++ counting_sheep.java
@@ -0,0 +1,12 @@
+class Kata {
+  public static String countingSheep(int num) {
+    
+    String text = " sheep...";
+    String result = "";
+    
+    for (int i = 1; i <=num; i++) {
+      result += i + text;
+    }
+    return result;
+  }
+}
