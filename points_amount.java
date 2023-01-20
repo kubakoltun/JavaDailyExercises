665f79f1ab0cd6f1bbe2b514ab825a1de0d7ade4
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-01-20 21:57:44 +0100

    Create points_amount.java

---
+++ points_amount.java
@@ -0,0 +1,15 @@
+public class TotalPoints {
+  
+    public static int points(String[] games) {
+      int sum = 0;
+      
+      for (String game : games) {
+        char x = game.charAt(0),
+             y = game.charAt(2);
+        
+        sum += (x > y ? 3 : x == y ? 1 : 0); 
+      }
+      
+      return sum;
+    }
+}
