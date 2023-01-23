e88a3271e212b89b3abee17491f4d09b1b4d5012
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-01-23 19:09:55 +0100

    Create x_o.java

---
+++ x_o.java
@@ -0,0 +1,18 @@
+public class XO {
+  
+  public static boolean getXO (String str) {
+    
+    int x = 0;
+    int o = 0;
+    
+    for (int i = 0; i < str.length(); i++) {
+      if (str.toUpperCase().charAt(i) == 'X')
+        x++;
+      if (str.toUpperCase().charAt(i) == 'O')
+        o++;
+    }
+    
+    return x == o;
+    
+  }
+}
