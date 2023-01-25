257daa28f2e08067d5a9dd97b1bc725964fcbe5e
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-01-25 16:32:51 +0100

    Create control_growth.java

---
+++ control_growth.java
@@ -0,0 +1,15 @@
+public class Kata{
+
+  public static int grow(int[] x){
+    
+    int y = 1;
+
+    for (int i = 0; i < x.length; i++) {
+      y *= x[i];
+    }
+    
+    return y;
+  
+  }
+
+}
