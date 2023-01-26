34c195c9b72bd6ee191ce57a4442861269efc2ba
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-01-26 19:00:06 +0100

    Create yes_no.java

---
+++ yes_no.java
@@ -0,0 +1,36 @@
+public class yes_no {
+    private static int count = 1;
+
+    public static void yesNoCrt(int[] dataArr, int digits, int index) {
+
+        if (index == digits) {
+            String str = "";
+            for (int i = 0; i < digits; i++) {
+                if (dataArr[i] % 2 == 0) {
+                    str = str + "yes, ";
+                }
+                else {
+                    str = str + "no, ";
+                }
+
+            }
+            System.out.println(str + " " + count);
+            count++;
+
+            return;
+        }
+
+        for (int i = 0; i < 2; i++) {
+            dataArr[index] = i;
+            yesNoCrt(dataArr, digits, index + 1);
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] ynTabHold = new int[4];
+        yesNoCrt(ynTabHold, 4, 0);
+    }
+}
