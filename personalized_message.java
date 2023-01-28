7ed708d0107586b24b2495b5ad73ba7d3b262464
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-01-28 11:41:04 +0100

    Create personalized_message.java

---
+++ personalized_message.java
@@ -0,0 +1,11 @@
+lass Kata {
+    static String greet(String name, String owner) {
+        
+      if (name.equals(owner)) {
+        return "Hello boss";
+      }
+      else {
+        return "Hello guest";
+      }
+    }
+}
