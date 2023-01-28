def979bd699d267b392979df4b595fa4f5fb5ff8
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-01-28 11:42:52 +0100

    Create sentence_smash.java

---
+++ sentence_smash.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class SmashWords {
+
+	public static String smash(String... words) {
+    String concat = "";
+    for (int i = 0; i < words.length; i++) {
+      if (i == 0) {
+        concat = words[i];
+      }
+      else {
+        concat = concat + " " + words[i];
+      }
+      
+    }
+    return concat;
+  }
+}
