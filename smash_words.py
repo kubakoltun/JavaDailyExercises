3c9e913c35115cf3ca030e3b47bc15618071e452
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-01-29 11:03:57 +0100

    Update and rename smash_words.py to CharProblem.java

--- smash_words.py
+++
@@ -1,8 +0,0 @@
-def smash(words):
-    concat = ''
-    for i in range(len(words)):
-        if i == 0:
-            concat = words[i]
-        else:
-            concat = concat + ' ' + words[i]
-    return concat
