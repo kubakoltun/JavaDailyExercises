e47dd6cf1e4644a375ce9ff700e29d805cb7560b
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-02-10 18:28:51 +0100

    Create array_sum.java

---
+++ array_sum.java
@@ -0,0 +1,9 @@
+public class array_sum {
+  public static double sum(double[] numbers) {
+    double sum = 0;
+    for (int i = 0; i < numbers.length; i++) {
+      sum += numbers[i];
+    }
+    return sum;
+  }
+}
