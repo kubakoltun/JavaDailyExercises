e23ebad3ae240b5c8ddd514ba41218616919f367
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-02-17 23:47:09 +0100

    Create line_numbering.java

---
+++ line_numbering.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class line_numbering {
+    public static List<String> number(List<String> lines) {
+      int g = 1;
+      for (int z = 0; z < lines.size(); z++) {
+        lines.set(z, g + ": " + lines.get(z));
+        g++;
+      }
+      
+        return lines;
+    }
+}
