6eea2ad67c5dbbf946f3558fa967affd1592cf85
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-02-20 20:22:33 +0100

    Create phone_number.java

---
+++ phone_number.java
@@ -0,0 +1,16 @@
+public class phone_number {
+  public static String createPhoneNumber(int[] numbers) {
+    String number = "(";
+    for (int i = 0; i < numbers.length; i++) {
+      number += numbers[i];
+      if (i == 2) {
+        number += ") ";
+      }
+      if (i == 5) {
+        number += "-";
+      }
+    }
+    
+    return number;
+  }
+}
