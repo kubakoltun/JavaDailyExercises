52ac1ef4840afd566fa3a0b57b53038a47c14d40
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-04-25 20:14:13 +0200

    Update linked_list_variations.java

--- linked_list_variations.java
+++ linked_list_variations.java
@@ -1,3 +1,121 @@
+class MyList<T> {
+    private ListNode head;
+    private int size;
+
+    public MyList() {
+        head = null;
+        size = 0;
+    }
+
+    public void add(T e) {
+        ListNode newNode = new ListNode(e);
+        if (head == null) {
+            head = newNode;
+        } else {
+            ListNode curr = head;
+            while (curr.next != null) {
+                curr = curr.next;
+            }
+            curr.next = newNode;
+        }
+        size++;
+        System.out.println("added " + e);
+    }
+
+    public void add(T e, int i) {
+        if (i < 0 || i > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        ListNode newNode = new ListNode(e);
+        if (i == 0) {
+            newNode.next = head;
+            head = newNode;
+        } else {
+            ListNode curr = head;
+            for (int j = 0; j < i-1; j++) {
+                curr = curr.next;
+            }
+            newNode.next = curr.next;
+            curr.next = newNode;
+        }
+        size++;
+        System.out.println("added " + e + " at index " + i);
+    }
+
+    public T delete(int i) {
+        if (i < 0 || i >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        T result;
+        if (i == 0) {
+            result = (T) head.value;
+            head = head.next;
+        } else {
+            ListNode curr = head;
+            for (int j = 0; j < i-1; j++) {
+                curr = curr.next;
+            }
+            result = (T) curr.next.value;
+            curr.next = curr.next.next;
+        }
+        size--;
+        System.out.println("value " + result + " at index " + i);
+        return result;
+    }
+
+    public T delete() {
+        if (head == null) {
+            throw new IndexOutOfBoundsException();
+        }
+        T result;
+        if (head.next == null) {
+            result = (T) head.value;
+            head = null;
+        } else {
+            ListNode curr = head;
+            while (curr.next.next != null) {
+                curr = curr.next;
+            }
+            result = (T) curr.value;
+            curr.next = null;
+        }
+        size--;
+        System.out.println("last value " + result);
+        return result;
+    }
+
+    public T show(int i) {
+        if (i < 0 || i >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        ListNode curr = head;
+        for (int j = 0; j < i; j++) {
+            curr = curr.next;
+        }
+        System.out.println("at index " + i + " value is " + (T) curr.value);
+        return (T) curr.value;
+    }
+
+    public void show() {
+        ListNode curr = head;
+        while (curr != null) {
+            System.out.println(curr.value + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+    private static class ListNode<T> {
+        T value;
+        ListNode next;
+
+        ListNode(T value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+}
+
+
 public class linked_list_variations {
 
     public static void main(String[] args) {
