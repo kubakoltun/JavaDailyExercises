16cfe3524d526c6602b9e2575a34a5da22cd7f0f
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-05-04 21:21:15 +0200

    Update sentence_smash.java

--- sentence_smash.java
+++ sentence_smash.java
@@ -1,18 +1,17 @@
 import java.util.Arrays;
 
 public class sentence_smash {
-
 	public static String smash(String... words) {
-    String concat = "";
-    for (int i = 0; i < words.length; i++) {
-      if (i == 0) {
-        concat = words[i];
-      }
-      else {
-        concat = concat + " " + words[i];
-      }
-      
-    }
+	    String concat = "";
+	    for (int i = 0; i < words.length; i++) {
+	      if (i == 0) {
+		concat = words[i];
+	      }
+	      else {
+		concat = concat + " " + words[i];
+	      }
+	    }
+		
     return concat;
   }
 }
