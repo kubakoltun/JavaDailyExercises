de5ebf4bc461104985c344b84ab04c5ad050d9b0
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-07-11 22:53:50 +0200

    Update counting_occurrences.java

--- counting_occurrences.java
+++ counting_occurrences.java
@@ -1,13 +1,13 @@
 public class counting_occurrences {
   public int countSheeps(Boolean[] arrayOfSheeps) {
-       
     int counter = 0;
-    
+  
     for (int i = 0; i < arrayOfSheeps.length; i++) {
       if (arrayOfSheeps[i] != null && arrayOfSheeps[i] == true) {
         counter++;
       }
     }
+    
     return counter;
   }
 }
