fc03e8eafb330012ef6b5e84c242feb9dae9bdc8
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-07-12 23:41:53 +0200

    Update duplicate_encoder.java

--- duplicate_encoder.java
+++ duplicate_encoder.java
@@ -4,10 +4,13 @@ public class duplicate_encoder {
             StringBuilder jstr = new StringBuilder();
             StringBuilder nstr = new StringBuilder();
             int countchar = 0;
+		
             for (int i = 0; i < word.length(); i++) {
                 istr.append(word.charAt(i));
+		    
                 for (int j = 0; j < word.length(); j++) {
                     jstr.append(word.charAt(j));
+			
                     if (istr.toString().toLowerCase().equals(jstr.toString().toLowerCase())) {
                       System.out.println(istr + "=" + jstr);
                         countchar++;
@@ -16,10 +19,10 @@ public class duplicate_encoder {
                 }
                 if (countchar > 1) {
                     nstr.append(")");
-                }
-                else {
+                } else {
                     nstr.append("(");
                 }
+		    
                 istr = new StringBuilder();
                 countchar = 0;
             }
