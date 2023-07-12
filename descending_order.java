7e05b6b3c7f5234c9cd9e911cd85333899a82593
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-07-12 23:40:41 +0200

    Update descending_order.java

--- descending_order.java
+++ descending_order.java
@@ -4,28 +4,33 @@ public class descending_order {
   public static int sortDesc(int num) {
     int length = 0;
     long temp = 1;
+    
     while (temp <= num) {
         length++;
         temp *= 10;
     }
+    
     int[] arr = new int[length];
     int i = 0;
     int sort = 0;
+    
     while(num != 0) {
       int tmp = num % 10;
       num = num / 10;
       arr[i] = tmp;
       i++;
     }
+    
     Arrays.sort(arr);
     int[] b = new int[arr.length];
-        int j = arr.length;
-        for (int k = 0; k < arr.length; k++) {
-            b[j - 1] = arr[k];
-            j = j - 1;
-        }
+    int j = arr.length;
+    
+    for (int k = 0; k < arr.length; k++) {
+         b[j - 1] = arr[k];
+         j = j - 1;
+    }
     for (int g = 0; g < b.length; g++) {
-      sort = 10*sort + b[g];
+        sort = 10*sort + b[g];
     }
     
     return sort;
