c433c2520b671d6cf3c0073187d4ef7661c547eb
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-07-13 19:32:45 +0200

    Update control_growth.java

--- control_growth.java
+++ control_growth.java
@@ -1,7 +1,5 @@
 public class control_growth {
-
   public static int grow(int[] x){
-    
     int y = 1;
 
     for (int i = 0; i < x.length; i++) {
@@ -9,7 +7,5 @@ public class control_growth {
     }
     
     return y;
-  
   }
-
 }
