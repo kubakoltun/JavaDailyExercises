6ceac92587d2945b9c1c0f6e6bb23b101a1b9392
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-07-14 22:14:24 +0200

    Update cat_moves.java

--- cat_moves.java
+++ cat_moves.java
@@ -2,6 +2,7 @@ public class cat_moves {
   public static int solution(int start, int finish) {
     int moves = 0;
     boolean hasJumped = false;
+    
     for (int i = start; i < finish; i++) {
       hasJumped = false;
       if (start+3%2==0 && start+3 <= finish) {
@@ -11,11 +12,11 @@ public class cat_moves {
       else if (start+3%2!=0 && start+3 <= finish) {
         start += 3;
         moves += 1;
-      }
-      else {
+      } else {
         start += 1;
         moves += 1;
       }
+      
       if (start == finish) {
         break;
       }
