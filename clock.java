1891303968e1e782fc7d708529c1fb389e7eff49
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-07-14 22:14:11 +0200

    Update clock.java

--- clock.java
+++ clock.java
@@ -1,7 +1,5 @@
-public class clock
-{
-  public static int Past(int h, int m, int s) 
-  {
+public class clock {
+  public static int Past(int h, int m, int s) {
     return (h * 3600000) + (m * 60000) + (s * 1000);
   }
 }
