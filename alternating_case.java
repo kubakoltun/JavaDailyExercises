199da3f09a23285a7079a1e6d016955f8e7d14c5
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-07-14 22:14:43 +0200

    Update alternating_case.java

--- alternating_case.java
+++ alternating_case.java
@@ -1,13 +1,12 @@
 public class alternating_case {
   public static String toAlternativeString(String string) {
     String news = "";
+    
     for (int i = 0; i < string.length(); i++) {
       if (Character.isUpperCase(string.charAt(i))) {
         news += Character.toLowerCase(string.charAt(i));
-      } 
-      else {
+      } else {
         news += Character.toUpperCase(string.charAt(i));
-
       }
     }
     
