cb845a6304fd0f60188918c34e0374195754e784
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-07-15 19:50:56 +0200

    Update name_comparator.java

--- name_comparator.java
+++ name_comparator.java
@@ -4,9 +4,16 @@ import java.util.Comparator;
 public class name_comparator {
   public static String[] lineupStudents(String students) { 
     String[] res = students.split(" ");
-    String[] lst = new String[res.length];
     
-    Arrays.sort(res, Comparator.comparingInt(String::length).reversed());
+    Arrays.sort(res, new Comparator<String>() {
+            @Override
+            public int compare(String name1, String name2) {
+                if (name1.length() == name2.length()) {
+                    return name2.compareTo(name1); 
+                }
+                return Integer.compare(name2.length(), name1.length()); 
+            }
+        });
     
     return res;
   }
