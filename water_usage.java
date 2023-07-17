9b9059defd386f4dd35c7605acee5ec3262a8bc3
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-07-17 23:48:26 +0200

    Update water_usage.java

--- water_usage.java
+++ water_usage.java
@@ -1,11 +1,7 @@
 public class water_usage  {
-
-  public int Liters(double time)  {
-    
+  public int Liters(double time)  {    
     int water = (int) Math.floor(time*0.5);
     
     return water;
-  
   }
-
 }
