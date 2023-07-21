03cfbed7c43f73103c2244163c18a4aa8a7f3f8b
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-07-21 19:53:50 +0200

    Update unflatten_list.java

--- unflatten_list.java
+++ unflatten_list.java
@@ -3,28 +3,24 @@ import java.util.List;
 
 public class Kata {
     public static Object[] unflatten(int[] arr) {
-      
-        
         List<Object> result = new ArrayList<>();
         int i = 0;
 
         while (i < arr.length) {
-            if (arr[i] < 3) {
-                result.add(arr[i]);
-                i++;
-            } else {
-                int subArraySize = arr[i];
-                Object[] subArray = new Object[subArraySize];
-
-                for (int j = 0; j < subArraySize && i + j + 1 < arr.length; j++) {
-                    subArray[j] = arr[i + j + 1];
+            int subArraySize = arr[i];
+            if (subArraySize > 2) {
+                List<Integer> subArray = new ArrayList<>();
+                for (int j = 1; j <= subArraySize && i + j < arr.length; j++) {
+                    subArray.add(arr[i + j]);
                 }
-
                 result.add(subArray);
                 i += subArraySize + 1;
+            } else {
+                result.add(arr[i]);
+                i++;
             }
         }
 
         return result.toArray();
     }
-  }
+}
