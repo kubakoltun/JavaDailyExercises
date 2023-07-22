e3f501d826e897b6b283126a5c3b00d5b8865ede
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-07-22 13:43:08 +0200

    Update nth_series.java

--- nth_series.java
+++ nth_series.java
@@ -1,15 +1,15 @@
-public class NthSeries {
-	public static String seriesSum(int n) {
+public class nth_series {
+  public static String seriesSum(int n) {
     double sum = 0;
     int xn = 0;
     double a = 0;
-		for (int i = 0; i < n; i++) {
+    
+    for (int i = 0; i < n; i++) {
       a += 1.0/(1+xn);
       xn += 3;
       sum = (double) Math.round(a * 100) / 100;
-      System.out.println(sum);
     }
     
-    return sum+"";
+    return String.format("%.2f", sum);
   }
 }
