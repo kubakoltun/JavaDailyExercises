0a1bc94c08b384cee5e353e7496f5aff28a3043a
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-07-24 20:21:45 +0200

    Update char_problem.java

--- char_problem.java
+++ char_problem.java
@@ -1,6 +1,7 @@
 public class char_problem {
   public static int howOld(final String herOld) {
     char age = ' ';
+    
     for (int i = 0; i < herOld.length(); i++){
       if (i == 0) {
         System.out.print(herOld.charAt(i));
