69d6c9696034c4b735050f4b85eef90cb4a25b6f
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-07-25 20:06:36 +0200

    Update count_by_x.java

--- count_by_x.java
+++ count_by_x.java
@@ -2,6 +2,7 @@ public class count_by_x {
   public static int[] countBy(int x, int n){
     int[] tab = new int[n];
     int tmp = x;
+    
     for (int i = 0; i < n; i++) {
       tab[i] =  tmp;
       tmp += x;
