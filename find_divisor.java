9a24113db355ef6e0734b7cc73851b03e4dceccf
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-07-25 20:08:16 +0200

    Update find_divisor.java

--- find_divisor.java
+++ find_divisor.java
@@ -1,25 +1,17 @@
 public class find_divisor {
-
   public long numberOfDivisors(int n) {
-    
     int counter = 0;
-    
     int x = 1;
     
     for (int i = 0; i < n; i++) {
-      
       if (n % x == 0) {
         counter++;
         x += 1;
       } else {
         x += 1;
       }
-    
-    
     }
     
     return counter;
-
   }
-
 }
