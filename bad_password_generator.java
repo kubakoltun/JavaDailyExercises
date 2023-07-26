c7cc1abc80525b1afe68fc017a19e8317dccca40
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-07-26 22:19:00 +0200

    Update bad_password_generator.java

--- bad_password_generator.java
+++ bad_password_generator.java
@@ -1,13 +1,15 @@
-public static String bad_password_generator(int rand_swt) {
-        String base = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz12345674890!_-&#$%";
-        java.util.Random rand = new java.util.Random();
-        StringBuilder builder = new StringBuilder();
-
-            while(builder.toString().length() == 0) {
-                for (int i = 0; i < rand_swt; i++) {
-                    builder.append(base.charAt(rand.nextInt(base.length())));
-                }
-            }
-  
-            return builder.toString();
-    }
+public class bad_password_generator {
+        public static String bad_password_generator(int rand_swt) {
+                String base = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz12345674890!_-&#$%";
+                java.util.Random rand = new java.util.Random();
+                StringBuilder builder = new StringBuilder();
+        
+                    while(builder.toString().length() == 0) {
+                        for (int i = 0; i < rand_swt; i++) {
+                            builder.append(base.charAt(rand.nextInt(base.length())));
+                        }
+                    }
+          
+                    return builder.toString();
+        }
+}
