a0deffc1b85638fe7cfb97af3d2490596cafdb35
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-07-28 17:32:15 +0200

    Update phone_number.java

--- phone_number.java
+++ phone_number.java
@@ -1,6 +1,7 @@
 public class phone_number {
   public static String createPhoneNumber(int[] numbers) {
     String number = "(";
+    
     for (int i = 0; i < numbers.length; i++) {
       number += numbers[i];
       if (i == 2) {
