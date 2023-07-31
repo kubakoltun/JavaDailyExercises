78727a3391780cbcf816491ef9717c69a0dacb4b
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-07-31 18:55:12 +0200

    Update find_wolf.java

--- find_wolf.java
+++ find_wolf.java
@@ -1,10 +1,10 @@
 import java.util.*;
-public class find_wolf {
 
+public class find_wolf {
   public static String warnTheSheep(String[] array) {
     Collections.reverse(Arrays.asList(array));
     int pos = Arrays.asList(array).indexOf("wolf");
+    
     return pos == 0 ? "Pls go away and stop eating my sheep" : "Oi! Sheep number " + pos + "! You are about to be eaten by a wolf!";
   }
-
 }
