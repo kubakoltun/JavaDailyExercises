a01524c16b9ae3846cf1946066689975ba8cb4cb
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-08-03 17:49:57 +0200

    Update reversed_strings.java

--- reversed_strings.java
+++ reversed_strings.java
@@ -1,7 +1,5 @@
 public class reversed_strings {
-
   public static String solution(String str) {
-    
     String out = "";
       
     for (int i = str.length() - 1; i >= 0; i--) {
@@ -10,5 +8,4 @@ public class reversed_strings {
     
     return out;
   }
-
 }
