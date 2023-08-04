d937c82e47cbfb45efae63af98129ca2fb44a059
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-08-04 16:33:14 +0200

    Update smallest_integer.java

--- smallest_integer.java
+++ smallest_integer.java
@@ -1,6 +1,7 @@
 public class smallest_integer {
     public static int findSmallestInt(int[] args) {
       int tmp = args[0];
+        
       for (int i = 0; i < args.length; i++) {
         if (args[i] < tmp) {
           tmp = args[i];
