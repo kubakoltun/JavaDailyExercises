c19529a3a36bb828c41016fc5c7ef7d20cf7e8a1
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-08-04 16:33:08 +0200

    Update reversed_array_of_digits.java

--- reversed_array_of_digits.java
+++ reversed_array_of_digits.java
@@ -3,6 +3,7 @@ public class reversed_array_of_digits {
       String tmep = Long.toString(n);
       int[] arr = new int[tmep.length()];
       int tlr = tmep.length()-1;
+    
       for (int i = 0; i < tmep.length(); i++) {
         arr[i] = tmep.charAt(tlr) - '0';
         tlr--;
