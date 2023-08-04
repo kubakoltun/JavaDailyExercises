8d2c927f29548deab022d06617afbaaa976b8f1f
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-08-04 16:29:36 +0200

    Update traffic_lights.java

--- traffic_lights.java
+++ traffic_lights.java
@@ -1,16 +1,15 @@
 public class traffic_lights {
-
   public static String updateLight(String current) {
-  
     if (current == "green") {
             current = "yellow";
-        } else if (current == "yellow") {
+    } 
+    else if (current == "yellow") {
             current = "red";
-        } else if (current == "red") {
+    } 
+    else if (current == "red") {
             current = "green";
-        }
+    }
     
     return current;
   }
-  
 }
