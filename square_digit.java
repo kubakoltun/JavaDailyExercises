c2a5dbfcb1c7ee07b1b8f3fe87d7c091ad44c833
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-08-04 16:33:34 +0200

    Update square_digit.java

--- square_digit.java
+++ square_digit.java
@@ -2,6 +2,7 @@ public class square_digit {
   public int squareDigits(int n) {
     String str = Integer.toString(n);
     String nstr = "";
+    
     for (int i = 0; i < str.length(); i++) {
       int nn = Character.getNumericValue(str.charAt(i));  
       nn *= nn;
