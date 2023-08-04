76086a85a256bdf711f42444908913145e179d00
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-08-04 16:31:06 +0200

    Update yes_no.java

--- yes_no.java
+++ yes_no.java
@@ -2,21 +2,18 @@ public class yes_no {
     private static int count = 1;
 
     public static void yesNoCrt(int[] dataArr, int digits, int index) {
-
         if (index == digits) {
             String str = "";
             for (int i = 0; i < digits; i++) {
                 if (dataArr[i] % 2 == 0) {
                     str = str + "yes, ";
-                }
-                else {
+                } else {
                     str = str + "no, ";
                 }
-
             }
+            
             System.out.println(str + " " + count);
             count++;
-
             return;
         }
 
@@ -27,7 +24,6 @@ public class yes_no {
     }
 
     public static void main(String[] args) {
-
         int[] ynTabHold = new int[4];
         yesNoCrt(ynTabHold, 4, 0);
     }
