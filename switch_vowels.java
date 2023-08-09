0382b4858e4ace7251ab949babff23473746110d
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-08-09 21:09:42 +0200

    Update switch_vowels.java

--- switch_vowels.java
+++ switch_vowels.java
@@ -1,15 +1,26 @@
-public class switch_vowels {
+public class Kata {
   public static String vowel2Index(String s) {
     char[] vowels =  {'a', 'e', 'i', 'o', 'u'};
     String result = "";
     
     for (int i = 0; i < s.length(); i++) {
-      if (s.charAt(i).includes(vowels)) {
+      if (includes(s.charAt(i), vowels)) {
         result += i+1;
       } else {
         result += s.charAt(i);
       }
     }
+    
     return result;
+  }
+  
+  public static boolean includes(char ct, char[] vow) {
+    for (char c : vow) {
+      if (c == ct) {
+        return true;
+      }
+    }
+    
+    return false;
   }
 }
