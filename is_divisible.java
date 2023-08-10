1eb68e9cb85ca13375017c64139ab74e94c6dea1
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-08-10 18:27:33 +0200

    Update is_divisible.java

--- is_divisible.java
+++ is_divisible.java
@@ -1,7 +1,11 @@
 public class is_divisible {
-    public static boolean isDivisible(int firstNum, int... divs) { 
-      for (int n : divs) {
-        if (firstNum % n != 0) {
+    public static boolean isDivisible(int... numbers) { 
+      int g = 0;
+      for (int n : numbers) {
+        if (g == 0) {
+          g = n;
+        }
+        if (g % n != 0) {
           return false;
         } 
       }
