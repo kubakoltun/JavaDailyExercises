e90fba4b925577179e229a266e223430bc15a461
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-08-16 19:52:07 +0200

    Create multiples_of_3_or_5.java

---
+++ multiples_of_3_or_5.java
@@ -0,0 +1,19 @@
+public class multiples_of_3_or_5 {
+  public int solution(int number) {
+    int sum = 0;
+    
+    for (int i = 0; i < number; i++) {
+      if (i % 3 == 0 && i % 5 != 0) {
+        sum += i;
+      }
+      else if (i % 3 != 0 && i % 5 == 0) {
+        sum += i;
+      }
+      else if (i % 15 == 0) {
+        sum += i;
+      }
+    }
+    
+    return sum;
+  }
+}
