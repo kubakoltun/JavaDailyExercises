9b5a5d9a40868bfb2ec901fdc5d94497fbe80fe2
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-08-30 20:07:48 +0200

    Update array_square_average.java

--- array_square_average.java
+++ array_square_average.java
@@ -2,7 +2,7 @@ public class array_square_average {
   public static double solution(int[] arr1, int[] arr2) {
     int length = arr1.length;
     int difSum = 0;
-    double avgHold = 0;
+    double avgHold = 0; 
     
     for (int i = 0; i < length; i++) {
       difSum = 0;
