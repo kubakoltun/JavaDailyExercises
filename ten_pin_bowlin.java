842bd5c85d8c1ea058338e741717cccbeac380f2
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-10-05 18:58:45 +0200

    Update and rename balanced_parens.js to ten_pin_bowlin.java

---
+++ ten_pin_bowlin.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Solution {
+
+    public static int bowling_score(String frames) {
+        String[] fArr = frames.split(" ");
+        int score = 0;
+        
+        for (int i = 0 ; i < fArr.length ; i++) {
+            if (fArr[i].matches("X|[0-9]/")) {
+                if (i < 9) fArr[i] = Arrays.stream(fArr, i, fArr.length)
+                                           .collect(Collectors.joining(""))
+                                           .substring(0, 3);
+            }
+            score += fArr[i].replaceAll("[0-9]/","X")
+                            .chars()
+                            .map( n -> !Character.isDigit((char) n) ? 10 : Integer.valueOf(Character.toString((char) n)))
+                            .sum();
+        }
+        return score;
+    }
+}
